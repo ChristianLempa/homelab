ada248d4871c6377997887eac0bb185fc54e11a0
Christian Lempa <christian.lempa@clcreative.de>
2024-05-06 16:41:31 +0200

    dns test 3

--- dns/dns-prod-1/terraform/eval.tf
+++ dns/dns-prod-1/terraform/eval.tf
@@ -6,3 +6,12 @@ resource "dns_a_record_set" "test" {
   ]
   ttl = 3600
 }
+
+resource "dns_a_record_set" "test2" {
+  zone = "home.clcreative.de."
+  name = "test2"
+  addresses = [
+    "8.8.8.8"
+  ]
+  ttl = 3600
+}
