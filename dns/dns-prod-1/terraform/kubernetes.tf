3aa519c75ded47942c6dfbfd1040cfa9764c2c38
Christian Lempa <christian.lempa@clcreative.de>
2024-07-22 17:15:54 +0200

    ip addresses for kube prod 2

--- dns/dns-prod-1/terraform/kubernetes.tf
+++ dns/dns-prod-1/terraform/kubernetes.tf
@@ -60,3 +60,23 @@ resource "dns_a_record_set" "nas_prod_1_lb_wildcard" {
   ]
   ttl = 3600
 }
+
+# --- kube-prod-2
+
+resource "dns_a_record_set" "kube_prod_2_lb" {
+  zone = "home.clcreative.de."
+  name = "kube-prod-2"
+  addresses = [
+    "10.20.21.10"
+  ]
+  ttl = 3600
+}
+
+resource "dns_a_record_set" "kube_prod_2_lb_wildcard" {
+  zone = "home.clcreative.de."
+  name = "*.kube-prod-2"
+  addresses = [
+    "10.20.21.21"
+  ]
+  ttl = 3600
+}
